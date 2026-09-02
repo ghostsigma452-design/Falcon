8287a13e5f0b74b27a1d276f87d73218dfacfdab
Sigma <ghostsigma452@gmail.com>
2026-09-02 22:54:39 +0530

    tgrtg

--- falcon.nim
+++ falcon.nim
@@ -1,5 +1,6 @@
 import src/[window, device, vkLoader, swapchain, renderpass, pipeline, command, buffer, descriptors, vulkanContext, helper, fmath]
 import vk14
 import sdl2
+import cglm
 
-export window, sdl2, vk14, device, vkLoader, swapchain, renderpass, pipeline, command, buffer, descriptors, vulkanContext, helper, fmath
+export window, sdl2, vk14, device, vkLoader, swapchain, renderpass, pipeline, command, buffer, descriptors, vulkanContext, helper, fmath, cglm
